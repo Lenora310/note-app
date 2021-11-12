6d5b42b0c662562542933a313f154aed1c8bb8fa
Elena Patceva <patceele@fit.cvut.cz>
2021-11-12 17:32:20 +0100

    Failed attempt to understand HOOKs

--- src/pages/TemplateCreator.js
+++ src/pages/TemplateCreator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
 export const TemplateCreator = () => {
@@ -8,43 +8,49 @@ export const TemplateCreator = () => {
     { parentId: 0, elementTag: "div", elementId: draftId, html: "div element" },
   ]);
   const [id, setId] = useState(100);
-  const [color, setColor] = useState("white");
-  const [selected, setSelected] = useState("");
-  const [hovered, setHovered] = useState("");
+  const [selected, setSelected] = useState(draftId);
+  const [hovered, setHovered] = useState(draftId);
   console.log("rendering...");
   // console.log("rendering... elements=", elements);
 
   function usePrevious(value) {
-    const ref = useRef();
+    console.log("value=", value);
+    const ref = useRef(value);
     useEffect(() => {
+      console.log("useEffect...");
       ref.current = value;
+      console.log("Use effect ref=", ref);
     });
+    console.log(" usePrevious ref=", ref);
     return ref.current;
   }
+  const prevState = usePrevious({ selected, hovered });
 
   useEffect(() => {
-  if (selected) {
-    let el = document.styleSheets[0].cssRules[0].style;
-    el.removeProperty("background-color");
-    // console.log("document=", document.styleSheets);
-    // console.log("el=", el);
+    if (selected) {
+      // console.log("prevState=", prevState);
+      const prevSelEl = document.getElementById(prevState.selected);
+      const prevSelLayer = document.getElementById(
+        prevState.selected + "-layer"
+      );
+      const selEl = document.getElementById(selected);
+      const selLayer = document.getElementById(selected + "-layer");
 
-    // let styles = `#${selected} { color: red }`;
-    let element = document.getElementById(selected);
-    console.log("element=", element);
-    element.style.backgroundColor = "red";
+      console.log("prevSelEl=", prevSelEl);
+      console.log("prevSelLayer=", prevSelLayer);
+      console.log("selEl=", selEl);
+      console.log("selLayer=", selLayer);
 
-    // let css = document.createElement("style");
-    // // css.type = "text/css";
+      prevSelEl.style.backgroundColor = "white";
+      prevSelLayer.style.color = "black";
 
-    // // if (css.styleSheet) css.styleSheet.cssText = styles;
-    // // else
-    // css.appendChild(document.createTextNode(styles));
-
-    // /* Append style to the tag name */
-    // element.appendChild(css);
-  }
+      selEl.style.backgroundColor = "green";
+      selLayer.style.color = "green";
+    }
   }, [selected]);
+  // useEffect(() => {
+
+  // }, [hovered]);
 
   function addElement(parentId, elementTag, elementId, html) {
     let parentElement = document.getElementById(parentId);
@@ -60,23 +66,6 @@ export const TemplateCreator = () => {
     setId(id + 1);
     return id;
   };
-  // const changeColor = () => {
-  //   // if(color==="white"){
-  //   //   setColor("black")
-  //   // } else {
-  //   //   setColor("white")
-  //   // }
-  //   const newColor = color === "white" ? "green" : "white";
-  //   // console.log(newColor);
-  //   setColor(newColor);
-  // };
-
-  // const changeColor = (id) => {
-  //   setSelected(id);
-  // };
-  // const changeHoverColor = (id) => {
-  //   setHovered(id);
-  // };
 
   return (
     <Container fluid>
@@ -105,10 +94,8 @@ export const TemplateCreator = () => {
         <Col className="template-creator-column">
           <strong> Layers </strong>
           <ul>
-
-
-          {elements.map((el) => (
-              <li id={el.elementId+"-layer"} key={el.elementId}>
+            {elements.map((el) => (
+              <li id={el.elementId + "-layer"} key={el.elementId}>
                 <p
                   onClick={() => setSelected(el.elementId)}
                   onMouseEnter={() => setHovered(el.elementId)}
@@ -119,37 +106,6 @@ export const TemplateCreator = () => {
                 </p>
               </li>
             ))}
-
-
-
-
-
-
-            {/* {elements.map((el) => (
-              <li id={el.elementId+"-layer"} key={el.elementId}>
-                <p
-                  style={{
-                    color:
-                      selected === el.elementId
-                        ? "green"
-                        : hovered === el.elementId
-                        ? "blue"
-                        : "",
-                  }}
-                  onClick={() => changeColor(el.elementId)}
-                  onMouseEnter={() => changeHoverColor(el.elementId)}
-                  // onMouseLeave={() => changeHoverColor(el.elementId)}
-                >
-                  <strong>
-                    {el.elementId} &nbsp; {el.html}{" "}
-                  </strong>
-                </p>
-              </li>
-              // <p>
-              //  {el.elementId} &nbsp; {el.html}
-              //</p>
-              //</div>
-            ))} */}
           </ul>
         </Col>
       </Row>
