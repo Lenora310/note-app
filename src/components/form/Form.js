27b9722ab1fc7abcb86e654bba88a4dbf38b722c
Elena Patceva <patceele@fit.cvut.cz>
2021-12-23 16:23:34 +0100

    Fight with git

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,32 +1,28 @@
 import React, { useState, useContext } from "react";
 import { AlertContext } from "../../context/alert/alertContext";
-import { FirebaseContext } from "../../context/firebase/firebaseContext";
 
-export const PageForm = ({bookId}) => {
+
+export const Form = ({saveValue, placeholder}) => {
   const [value, setValue] = useState("");
   const alert = useContext(AlertContext);
-  const firebase = useContext(FirebaseContext);
 
   const submitHandler = (event) => {
     event.preventDefault();
     if (value.trim()) {
-        firebase.addPage(bookId, value.trim()).then( () => {
-            alert.show("Page was added", "success");
-        }).catch( () => {
-          alert.show("Something went wrong", "danger");
-        })
+        saveValue(value.trim());
     } else {
       alert.show("Enter text");
     }
-    setValue("");
+    setValue("")
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
           type="text"
           className="form-control"
-          placeholder="Write page title"
+          placeholder={placeholder}
           value={value}
           onChange={(event) => setValue(event.target.value)}
         ></input>
---
+++ src/pages/authentication/SignUp.js
@@ -0,0 +1,86 @@
+import React, { useContext, useState, useEffect } from "react";
+import { AlertContext } from "../../context/alert/alertContext";
+import { FirebaseContext } from "../../context/firebase/firebaseContext";
+import {
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { Button } from "react-bootstrap";
+
+
+export const SignUp = () => {
+  const alert = useContext(AlertContext);
+  const firebase = useContext(FirebaseContext);
+
+  const [loginValue, setLoginValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+  const [passwordCopyValue, setPasswordCopyValue] = useState("");
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    if (!loginValue.trim() || !passwordValue || !passwordCopyValue) {
+      alert.show("Fill all fields please");
+      return;
+    }
+    if (passwordValue !== passwordCopyValue) {
+      alert.show("Passwords should be the same");
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, loginValue, passwordValue)
+      .then(() => {
+        firebase.addUser(auth.currentUser)
+        alert.show("User was created", "success");
+      })
+      .catch(() => {
+        alert.show("Something went wrong", "danger");
+      });
+  };
+
+  return (
+    <form className="sign-up-form" onSubmit={signUp}>
+      <h1 className="h3 mb-3 fw-normal">Sign up</h1>
+
+      <div className="form-floating">
+        <input
+          type="email"
+          className="form-control"
+          id="floatingInput"
+          placeholder="login"
+          onChange={(event) => setLoginValue(event.target.value)}
+        />
+        <label htmlFor="floatingInput">Email</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          onChange={(event) => setPasswordValue(event.target.value)}
+        />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPasswordCopy"
+          placeholder="Repeat password"
+          onChange={(event) => setPasswordCopyValue(event.target.value)}
+        />
+        <label htmlFor="floatingPasswordCopy">Repeat password</label>
+      </div>
+
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <Button className="w-100 btn btn-lg btn-primary" type="submit">
+        Sign up
+      </Button>
+    </form>
+  );
+};
